e786cfe4abead5c7a00a9757167b52ffd03bad19
Ata Türkoğlu <ataturkoglu2003@hotmail.com>
2021-06-17 22:27:33 +0300

    Tweaks & README
    - Added README.md.
    - Added buttons for low and high res modes.
    - Increased the size of the canvas.
    - Added center to mouse position on left click.

---
+++ README.md
@@ -0,0 +1,22 @@
+# Mandelbrot Explorer
+
+This project uses p5.js to visualize [the Mandelbrot Set](https://en.wikipedia.org/wiki/Mandelbrot_set) in an interactive fashion. [Here](https://atatrkgl.github.io/mandelbrot-explorer/) is the link to the site if you want to try it out.
+
+## Controls
+
+- *Scrollwheel:* Zoom in/out.
+- *Left-click:* Center the mouse position.
+- *Slider:* Changes the maximum allowed iterations. (Effects performance heavily.)
+- *Resolution  Buttons:* Allows you to toggle between pixelated and detailed versions.
+
+> Note that it is recommended to use the low-res mode while moving so the site can refresh more frequently, then you can activate the high-res mode to generate the detailed version.
+
+## Some Screenshots
+
+![ss 1](https://i.imgur.com/zwxydeu.png)
+
+![ss 2](https://i.imgur.com/2UhrEtV.png)
+
+![ss 3](https://i.imgur.com/bsMSYGF.png)
+
+> You can right-click and use the "Save image as..." to save these through the site.
--- sketch.js
+++ sketch.js
@@ -1,47 +1,47 @@
 
 let iterationsSlider;
-let sliderLastVal;
-let zoom = 300;
-let origin = [.2, .3];
+let iterApplyButton;
+let iterations = 50;
 
-let pixelSkipCount = 0;
-let timeSinceZoom = 0;
+let lowResButton;
+let highResButton;
+
+let zoom = 300;
+let origin = [0, 0];
+
+let pixelSkipCount = 9;
 
 function setup() 
 {
-	createCanvas(800, 800);
+	createCanvas(displayWidth - 20, displayHeight - 200);
     background(0);
     pixelDensity(1);
     frameRate(60);
 
-    iterationsSlider = createSlider(5, 100, 30, 1);
+    iterationsSlider = createSlider(5, 300, iterations, 1);
+    iterApplyButton = createButton("Apply Iterations");
+    iterApplyButton.mousePressed(applyIterations);
+
+    highResButton = createButton("High Res");
+    highResButton.mousePressed(() => {pixelSkipCount = 1; drawMandelbrot();});
+    lowResButton = createButton("Low Res");
+    lowResButton.mousePressed(() => {pixelSkipCount = 9; drawMandelbrot();});
 
     drawMandelbrot();
 }
 
-function draw()
-{
-    timeSinceZoom += deltaTime / 1000;
-    if (sliderLastVal != iterationsSlider.value())
-        drawMandelbrot();
-
-    sliderLastVal = iterationsSlider.value();
-    if (timeSinceZoom > 5 && pixelSkipCount != 0)
-    {
-        pixelSkipCount = 0;
-        drawMandelbrot();
-    }
-    else if (timeSinceZoom < 5 && pixelSkipCount == 0)
-        pixelSkipCount = 7;
+function applyIterations(){
+    iterations = iterationsSlider.value();
+    drawMandelbrot();
 }
 
 function iterateNumber(real, imag){
     let c = new Complex(real, imag);
     let z = new Complex(0, 0);
-    for (let i = 0; i < iterationsSlider.value(); i++) {
+    for (let i = 0; i < iterations; i++) {
         z = Complex.add(Complex.multiply(z, z), c);
         if (abs(z.re) + abs(z.im) > 10){
-            return i/iterationsSlider.value() * 255 * 3;
+            return i/iterations * 255 * 3;
         }
     }
 
@@ -52,16 +52,27 @@ function drawMandelbrot(){
     background(0);
     loadPixels();
 
-    for (var y = 0; y < height; y+=1 + pixelSkipCount) {
-        for (var x = 0; x < width; x+=1 + pixelSkipCount){
-            coords = pixelToCoords([x, y]);
-            let iteratedColor = iterateNumber(coords[0], coords[1]);
+    for (var y = 0; y < height; y++) {
+        for (var x = 0; x < width; x++){
             let i = (y * width + x) * 4;
-            
-            pixels[i + 0] = clamp(iteratedColor - 255 * 2, 0 ,255);
-            pixels[i + 1] = clamp(iteratedColor - 255 * 1, 0 ,255);
-            pixels[i + 2] = clamp(iteratedColor, 0 ,255);
-            pixels[i + 3] = 255;
+            if (x % pixelSkipCount == 0 && y % pixelSkipCount == 0){
+                coords = pixelToCoords([x, y]);
+                let iteratedColor = iterateNumber(coords[0], coords[1]);
+
+                pixels[i + 0] = clamp(iteratedColor - 255 * 2, 0 ,255);
+                pixels[i + 1] = clamp(iteratedColor - 255 * 1, 0 ,255);
+                pixels[i + 2] = clamp(iteratedColor, 0 ,255);
+                pixels[i + 3] = 255;
+            }else{
+                yToCopy = y - (y % pixelSkipCount);
+                xToCopy = x - (x % pixelSkipCount);
+                let iToCopy = (yToCopy * width + xToCopy) * 4;
+
+                pixels[i + 0] = pixels[iToCopy + 0];
+                pixels[i + 1] = pixels[iToCopy + 1];
+                pixels[i + 2] = pixels[iToCopy + 2];
+                pixels[i + 3] = 255;
+            }
         }
     }
     updatePixels();
@@ -74,8 +85,8 @@ function pixelToCoords(pixelCords, zoomAmount=zoom){
 
 function mouseWheel(event) {
     let prevZoom = zoom;
-    zoom -= event.delta * 10;
-    if (zoom < 100) zoom = 100;
+    zoom -= event.delta * zoom / 300;
+    if (zoom < 200) zoom = 200;
 
     // By changing the origin by the position delta of the cursor
     // we can keep the mouse position still while zooming.
@@ -87,7 +98,16 @@ function mouseWheel(event) {
     origin[1] -= currentPos[1] - lastPos[1];
 
 
-    timeSinceZoom = 0;
+    drawMandelbrot();
+}
+
+function mouseClicked() {
+    if (mouseX < 0 || mouseX > width || mouseY < 0 || mouseY > height) return;
+
+    let mousePos = [mouseX, mouseY];
+    let mouseCoords = pixelToCoords(mousePos);
+    origin = mouseCoords;
+
     drawMandelbrot();
 }
 
